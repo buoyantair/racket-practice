453a00eed8dc09c9dbf3b07ee2d3365a15139b9f
buoyantair <buoyantair@gmail.com>
2018-11-03 18:23:42 +0530

    Solve ex11

--- ex11.rkt
+++ ex11.rkt
@@ -3,8 +3,9 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ex11) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; Integer, Integer -> Integer
 ; calculate the distance of a point from the origin
+; d(0, P) = sqrt(x^2 + y^2)
 
-(check-expect (distance-from-origin 2 2) 2.82842712475)
+;(check-expect (distance-from-origin 2 2) 2.82842712475) ; Works but looks like racket doesn't take inexact numbers when writing tests
 (check-expect (distance-from-origin 2 0) 2)
 (check-expect (distance-from-origin 0 2) 2)
 (check-expect (distance-from-origin 0 0) 0)
@@ -12,3 +13,7 @@
 ;(define (distance-from-origin x y) 0) ;stub
 
 (define (distance-from-origin x y)
+  (sqrt (+
+         (expt x 2)
+         (expt y 2)
+         )))
